2d4f0f8ae7e99c9fd803d4879613952d239ef115
MrAlex31 <brian-alexix@hotmail.com>
2021-08-23 20:27:34 -0300

    revirtiendo cambios

--- apps/appPrincipal/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class AppprincipalConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'apps.appPrincipal'
--- django_site/settings/local.py
+++ django_site/settings/local.py
@@ -7,10 +7,18 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'django_site',
+<<<<<<< HEAD
         'USER': '',
         'PASSWORD':'',
         'HOST':'localhost',
         'PORT':'3306',
     
+=======
+        'USER': 'root',
+        'PASSWORD':'Alexix31',
+        'HOST':'localhost',
+        'PORT':'3306',
+        
+>>>>>>> parent of 07800df ( - agregué settings/local a .gitignore para que no se guarden las configuraciones de base de datos de cada uno (se veia la contraseña de root))
     }
 }
