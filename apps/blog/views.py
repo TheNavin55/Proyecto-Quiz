aef424f69582c23e5f2b165e5b472c7ed3e74f49
nicolasDajruch <nicolasdajruch@gmail.com>
2021-08-31 17:58:47 -0300

    commit 31/08/21: - Agregué la lógica (archivo views) y el template jugar para que se vean las preguntas y respuestas en la página;

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -1,9 +1,14 @@
+from apps.appPrincipal.models import Pregunta
 from django.shortcuts import render
 from django.http import HttpResponse
+import random
 
 # Create your views here.
 def jugar(request):
-    return render(request, 'jugar.html',{})
+    listaIds = Pregunta.objects.filter(descripcion__isnull = False).values_list('id', flat = True)
+    listaIdsRandom = random.sample(list(listaIds), 4)
+    preguntas = Pregunta.objects.filter(id__in = listaIdsRandom)
+    return render(request, 'jugar.html',{'preguntas': preguntas})
 
 def login(request):
     return render(request, 'login.html',{})
