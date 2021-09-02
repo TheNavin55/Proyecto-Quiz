e56d8bf277368e36d72acd445f17d4344cfe3761
nicolasDajruch <nicolasdajruch@gmail.com>
2021-09-02 02:13:04 -0300

    COMMIT 02/09/21: - modifique la logica (views) y el template (jugar) para que las respuestas sean desordenadas (oredenadas de manera aleatoria);

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -5,10 +5,31 @@ import random
 
 # Create your views here.
 def jugar(request):
+    context = {}
     listaIds = Pregunta.objects.filter(descripcion__isnull = False).values_list('id', flat = True)
     listaIdsRandom = random.sample(list(listaIds), 4)
-    preguntas = Pregunta.objects.filter(id__in = listaIdsRandom)
-    return render(request, 'jugar.html',{'preguntas': preguntas})
+    preguntasRandom = Pregunta.objects.filter(id__in = listaIdsRandom).values()
+    preguntas = []
+    for i, item in enumerate(preguntasRandom, start=1):
+        pregunta = {}
+        pregunta['descripcion'] = item['descripcion']
+        pregunta['categoria'] = item['categoria']
+        pregunta['respuestas'] = []
+        rtas = [item['respuestaCorrecta'], item['incorrecta1'], item['incorrecta2'], item['incorrecta3']]
+        rtas2 = list(enumerate(rtas, start=1))
+        random.shuffle(rtas2)
+        for valor in rtas2:
+            respuesta = {}
+            respuesta['texto'] = valor[1]
+            if valor[0] != 1:
+                respuesta['correcto'] = False
+            else:
+                respuesta['correcto'] = True
+            respuesta['inputTagname'] = 'q_answer' + str(i)
+            pregunta['respuestas'].append(respuesta)
+        preguntas.append(pregunta)
+    context['preguntas'] = preguntas
+    return render(request, 'jugar.html', context)
 
 def login(request):
     return render(request, 'login.html',{})
@@ -23,4 +44,5 @@ def menu(request):
     return render(request, 'menu.html',{})
 
 def resultados(request):
-    return render(request, 'resultados.html',{})
+    if request.method == 'POST':
+        return render(request, 'resultados.html',{})
