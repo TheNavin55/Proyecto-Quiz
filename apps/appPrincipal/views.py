61f97dd08d2e9b2c8bdfe38927ee5b268b6403e7
nicolasDajruch <nicolasdajruch@gmail.com>
2021-09-07 19:29:43 -0300

    Incorporada logica para guardar partidas

--- apps/appPrincipal/views.py
+++ apps/appPrincipal/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib import messages
-from django.contrib.auth import authenticate, get_user_model, login as logear, logout as salir
+from django.contrib.auth import authenticate, login as logear, logout as salir
 from django.contrib.auth.decorators import login_required
 import random
 
@@ -40,19 +40,19 @@ def jugar(request):
 @login_required(login_url='login')
 def resultados(request):
     if request.method == 'POST':
-        print('esto es un POST')
         context = {}
-        usuario = get_user_model()
         nombre = request.user
         aciertos = 0
         porcentaje = 0
-        respuestas = [request.POST.get('q_answer1'), request.POST.get('q_answer2'),
-            request.POST.get('q_answer3'), request.POST.get('q_answer4')]
+        respuestas = [
+            request.POST.get('q_answer1'), request.POST.get('q_answer2'),
+            request.POST.get('q_answer3'), request.POST.get('q_answer4')
+        ]
+        print(respuestas)
         idPreguntas = [
             request.POST.get('id1'), request.POST.get('id2'),
             request.POST.get('id3'), request.POST.get('id4')
         ]
-        print(idPreguntas)
         preguntas = list(Pregunta.objects.filter(id__in = idPreguntas))
         for respuesta in respuestas:
             if respuesta == 'True':
@@ -62,20 +62,15 @@ def resultados(request):
             puntaje = porcentaje
         )
         juegoActual.save()
-        print('guardando objectos juega...')
         i = 0
-        print(preguntas)
         for pregunta in preguntas:
-            print('guardando usuario ' + str(i))
             Juega.objects.create(
-                idParticipante = usuario,
+                idParticipante = request.user,
                 idJuego = juegoActual,
                 idPregunta = pregunta,
-                correcto = bool(respuestas[i])
+                correcto = respuestas[i]
             )
-            print('deberia haberse creado un objecto')
             i += 1
-        print('estoy despues del for')
         context['participante'] =  nombre
         context['aciertos'] = aciertos
         context['porcentaje'] = porcentaje
