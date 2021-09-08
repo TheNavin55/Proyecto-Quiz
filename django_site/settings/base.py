23a93b5ac3b637b7f50b19497eff1785b7632eb0
nicolasDajruch <nicolasdajruch@gmail.com>
2021-09-07 23:12:39 -0300

    heroku 3

--- django_site/settings/base.py
+++ django_site/settings/base.py
@@ -131,7 +131,6 @@ STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(os.path.dirname(BASE_DIR), 'static'),
-    os.path.join(os.path.dirname(BASE_DIR), 'staticfiles'),
 ),
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), 'media')
